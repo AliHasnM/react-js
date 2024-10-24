921ed837d2555d9d9c79d88acae9a972db150f91
AliHasnM <mughal201burewala@gmail.com>
2024-10-24 13:32:56 +0500

    Initial Setup

---
+++ reduxToolkitTodoApp/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <div className="bg-zinc-900">
+        <h1 className="text-3xl text-white font-bold underline">
+          Learn About Redux Toolkit Todo App
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
