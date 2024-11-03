acc79ecc655d974b304f264937da1f47fafb9f50
AliHasnM <mughal201burewala@gmail.com>
2024-11-03 21:38:15 +0500

    Update Config File

--- megaBlogWithAppwrite/src/config/config.js
+++ megaBlogWithAppwrite/src/config/config.js
@@ -1,9 +1,16 @@
+// Define configuration settings for the Appwrite API, Project, Database, Collection, and Storage Bucket
 const config = {
+  // Sets the API endpoint URL, needed for connecting to the correct Appwrite server
   appwriteApiEndpoint: String(import.meta.env.VITE_APPWRITE_API_ENDPOINT),
+  // Project ID, ensures requests are scoped to the specific Appwrite project
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  // Database ID, identifies the database to access within Appwrite
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  // Collection ID, used to interact with specific collections in the database
   appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  // Bucket ID, identifies the storage bucket for handling file uploads and downloads
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 };
 
+// Exports the config object to be used throughout the app
 export default config;
